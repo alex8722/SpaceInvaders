798d9af5e7bee7d7bbec058508e955bb97da78be
Aurélien <tricardaurelien@gmail.com>
2018-05-06 16:41:55 +0200

    Les premiers tests

---
+++ src/main/java/fr/unilim/spaceinvaders/HorsEspaceJeuException.java
@@ -0,0 +1,10 @@
+package fr.unilim.spaceinvaders;
+
+@SuppressWarnings("serial")
+public class HorsEspaceJeuException extends RuntimeException {
+
+	public HorsEspaceJeuException(String message) {
+		super(message);
+	}
+
+}
